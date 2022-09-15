b2f83303826f10b0b8ac00e2f74f9d7215d6888d
Victoria <vica-genrietka@yandex.ru>
2022-09-15 22:54:27 +0300

    Написала программу, выполняющую программу

---
+++ Zadacha/Program.cs
@@ -0,0 +1,10 @@
+﻿string [] array = {"Hello" , "hi" , "cat", "pi"};
+
+string [] NewArray = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+   if (array[i].Length <= 3) 
+    NewArray [i] = array [i];
+    Console.WriteLine(NewArray[i]);
+}
+Console.WriteLine();
